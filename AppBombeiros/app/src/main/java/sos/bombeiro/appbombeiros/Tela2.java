52a5887bf9d74020f62d206db5b5d4c1a17e57ba
Gabriel Alexandre <gabrielalexandre100398@gmail.com>
2016-08-15 07:45:24 -0400

    Novo metodo de GPS

--- AppBombeiros/app/src/main/java/sos/bombeiro/appbombeiros/Tela2.java
+++ AppBombeiros/app/src/main/java/sos/bombeiro/appbombeiros/Tela2.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.IOException;
 import java.lang.Override;
@@ -30,7 +35,11 @@ public class Tela2 extends AppCompatActivity {
     private EditText edlatitude, edlongitude;
     private Button sos;
     private String s;
-
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
 
     @Override
@@ -39,64 +48,28 @@ public class Tela2 extends AppCompatActivity {
         setContentView(R.layout.activity_tela2);
 
 
+        sos = (Button) findViewById(R.id.sos);
+        sos.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
 
-            sos = (Button) findViewById(R.id.sos);
-            sos.setOnClickListener(new Button.OnClickListener() {
-                public void onClick(View v) {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        HttpHelper enviaDados = new HttpHelper();
+                        try {
+                            s = enviaDados.doPost("http://10.0.2.2:8080/ExemploWebService/rest/ocorrencia/todas", Tela1.conectar(), "UTF-8");
+                            GPS gps = new GPS(null);
+                            gps.ativaGPS();
+                        } catch (IOException e) {
 
-                    new Thread(){
-                        @Override
-                        public void run(){
-                            HttpHelper enviaDados = new HttpHelper();
-                            try{
-                                s = enviaDados.doPost("http://10.0.2.2:8080/ExemploWebService/rest/ocorrencia/todas", Tela1.conectar(), "UTF-8");
-                            }catch(IOException e){
-
-                            }
                         }
-                    }.start();
-                    Toast.makeText(getApplicationContext(), "Ocorrencia enviada.", Toast.LENGTH_SHORT).show();
-
-
-                    startGPS();
-                }
-            });
+                    }
+                }.start();
+                Toast.makeText(getApplicationContext(), "Ocorrencia enviada.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
-    // Método usado para importar os elementos da classe R
-
-
-    //Método que faz a leitura de fato dos valores recebidos do GPS
-    public void startGPS() {
-        LocationManager lManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        LocationListener lListener = new LocationListener() {
-            public void onLocationChanged(Location locat) {
-                updateView(locat);
-            }
-
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-            }
-
-            public void onProviderEnabled(String provider) {
-            }
-
-            public void onProviderDisabled(String provider) {
-            }
-        };
-
-        lManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, lListener);
-    }
-
-    //  Método que faz a atualização da tela para o usuário.
-    public void updateView(Location locat) {
-        Double latitude = locat.getLatitude();
-        Double longitude = locat.getLongitude();
-
-        edlatitude.setText(latitude.toString());
-        edlongitude.setText(longitude.toString());
-    }
-
-
 }
 
 
